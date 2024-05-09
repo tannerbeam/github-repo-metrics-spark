8554b5b45e7bad3d7827303595d4b144c554ba6b
Tanner Beam <100633974+tannerbeam@users.noreply.github.com>
2024-05-09 14:55:46 -0700

    add new org/repo to config file (#7)
    Co-authored-by: tannerbeam <tannerbeam>

--- config/etl.py
+++ config/etl.py
@@ -2,6 +2,6 @@ etl_config = {
     "workspace_directory": "Repos/prod/github-api",
     "github_secret_scope": "github",
     "github_secret_key": "tiny-tim-bot",
-    "include_github_orgs": ["greatexpectationslabs"],
-    "include_github_repos": ["mercury-web", "mercury"]
+    "include_github_orgs": ["greatexpectationslabs", "great-expectations"],
+    "include_github_repos": ["mercury-web", "mercury", "cloud"]
 }
